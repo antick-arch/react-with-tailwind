d3378ccbbfc11b53d1c4b817e15cd954a7869b1d
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-22 13:16:54 +0600

    added resposive navbar dynamically

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navlink from './Navlink';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
-    const navItems = [
-  {
-    id: 1,
-    name: "Home",
-    link: "#home"
-  },
-  {
-    id: 2,
-    name: "About",
-    link: "#about"
-  },
-  {
-    id: 3,
-    name: "Services",
-    link: "#services"
-  },
-  {
-    id: 4,
-    name: "Projects",
-    link: "#projects"
-  },
-  {
-    id: 5,
-    name: "Blog",
-    link: "#blog"
-  },
-  {
-    id: 6,
-    name: "Contact",
-    link: "#contact"
-  }
-];
-    return (
-        <nav className='flex justify-between mx-3'>
-            <h2 className='text-2xl font-extrabold'>TESTLOGO</h2>
-            <ul className='flex gap-7'>
-                {
-                    navItems.map(NavMenu => (
-                        <Navlink NavMenu = {NavMenu}></Navlink>
-                    ))
-                }
-            </ul>
-            <button className='btn'>Explore More</button>
-        </nav>
-    );
+  const [Open, setOpen] = useState(false);
+  const navItems = [
+    {
+      id: 1,
+      name: "Home",
+      link: "#home"
+    },
+    {
+      id: 2,
+      name: "About",
+      link: "#about"
+    },
+    {
+      id: 3,
+      name: "Services",
+      link: "#services"
+    },
+    {
+      id: 4,
+      name: "Projects",
+      link: "#projects"
+    },
+    {
+      id: 5,
+      name: "Blog",
+      link: "#blog"
+    },
+    {
+      id: 6,
+      name: "Contact",
+      link: "#contact"
+    }
+  ];
+  const links = navItems.map(NavMenu => (
+    <Navlink key={NavMenu.id} NavMenu={NavMenu}></Navlink>
+  ))
+
+  return (
+    <nav className='flex justify-between mx-3 mt-4'>
+      <span onClick={() => setOpen(!Open)} className='flex items-center'>
+
+        {
+          Open ? <X className='md:hidden' /> : <Menu className='md:hidden' />
+        }
+        <ul className={`md:hidden absolute ${Open? 'top-13':'-top-68'} bg-blue-300 duration-1000`}>
+          {links}
+        </ul>
+        <h2 className='text-2xl font-extrabold'>TESTLOGO</h2>
+      </span>
+
+      <ul className='hidden md:flex gap-7 items-center'>
+        {links}
+      </ul>
+
+      <button className='btn'>Explore More</button>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/Navbar/Navlink.jsx
+++ src/components/Navbar/Navlink.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
 const Navlink = ({NavMenu}) => {
     return (
-        <li>
-            <a href={NavMenu.link}>{NavMenu.name}</a>
+        <li className='p-1 border border-white hover:bg-red-500 duration-700 text-center'>
+            <a href={NavMenu.link} className='text-black font-bold'>
+                {NavMenu.name}
+                </a>
         </li>
     );
 };
