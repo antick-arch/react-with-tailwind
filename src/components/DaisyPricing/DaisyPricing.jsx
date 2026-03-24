0673a2574f721756e0c7d42efb5531b7a59f7459
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-24 17:34:17 +0600

    added rechart

--- src/components/DaisyPricing/DaisyPricing.jsx
+++ src/components/DaisyPricing/DaisyPricing.jsx
@@ -1,9 +1,27 @@
 import React, { use } from 'react';
 import DaisyFeatures from './DaisyFeatures';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
 
 const DaisyPricing = ({ pricingPromise }) => {
     const pricingData = use(pricingPromise).gym_pricing;
+    const data = [
+  { month: "Jan", income: 500, expense: 300 },
+  { month: "Feb", income: 600, expense: 350 },
+  { month: "Mar", income: 700, expense: 400 },
+  { month: "Apr", income: 650, expense: 420 },
+  { month: "May", income: 800, expense: 500 }
+];
     return (
+        <>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 place-self-center mb-10'>
             {
                 pricingData.map(pricingInfo => (
@@ -29,6 +47,23 @@ const DaisyPricing = ({ pricingPromise }) => {
                 ))
             }
         </div >
+        <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+
+            <XAxis dataKey="month" />
+            <YAxis />
+
+            <Tooltip />
+            <Legend />
+
+            <Line type="monotone" dataKey="income" stroke="#8884d8" />
+            <Line type="monotone" dataKey="expense" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+        </>
     );
 };
 
